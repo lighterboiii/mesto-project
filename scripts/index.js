d9c00205d92c69e788e8849d20e998f5431a63c6
Vyacheslav Baumtrok <s.baumtrok2009@yandex.ru>
2022-07-15 16:55:39 +0300

    добавил функционал открытия-закрытия окна элемента

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,14 @@
-let profileButton = document.querySelector('.profile__button');
-let popup = document.querySelector('.popup');
-let closeButton = document.querySelector('.form__close-button');
-let submitButton = document.querySelector('.form__button');
-let profileContainer = document.querySelector('.profile__container');
+const profileButton = document.querySelector('.profile__button');
+const popup = document.querySelector('.popup');
+const closeButton = document.querySelector('.form__close-button');
+const submitButton = document.querySelector('.form__button');
+const profileContainer = document.querySelector('.profile__container');
 let profileName = document.querySelector('.profile__name');
 let profileCaption = document.querySelector('.profile__caption');
+const addPhotoButton = document.querySelector('.add-button')
+const photoPopUp = document.querySelector('.popup__photo-container')
+const closePopUpButton = document.querySelector('.close-popup');
+
 
 
 function popupOpen() {
@@ -28,34 +32,16 @@ function formSubmitHandler(evt) {
 
   popupClose();
 }
+function addPhoto() {
+  photoPopUp.classList.add('popup_opened');
+}
+function closePopUp() {
+  photoPopUp.classList.remove('popup_opened');
+}
+
 
 profileButton.addEventListener('click', popupOpen);
 closeButton.addEventListener('click', popupClose);
 submitButton.addEventListener('click', formSubmitHandler);
-// Массив для создания 6 карточек при загрузке страницы. Взять другие картинки с unsplash
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-  ];
+addPhotoButton.addEventListener('click', addPhoto);
+closePopUpButton.addEventListener('click', closePopUp);
