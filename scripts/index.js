683a63b5347744a29f17aeecd16a90378c726d6d
Vyacheslav Baumtrok <s.baumtrok2009@yandex.ru>
2022-07-26 15:41:19 +0300

    исправил ошибки после 2 итерации

--- scripts/index.js
+++ scripts/index.js
@@ -24,7 +24,7 @@ const initialCards = [
     link: 'https://images.unsplash.com/photo-1658170213269-dc3aa8f27d0e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
   }
 ];
-
+// константы
 const profileButton = document.querySelector('.profile__button'); // кнопка открытия модального окна редактирования профиля
 const profilePopup = document.querySelector('.popup__profile'); // модальное окно редактирования профиля (заменил имя переменной с popup и нашел эл-т по уникальному селектору)
 const profileCloseButton = document.querySelector('.profile-close-button'); // кнопка закрытия окна редактирования профиля (заменил имя переменной и селектор поиска на более уникальный)
@@ -42,28 +42,19 @@ const openedImageCaption = document.querySelector('.photo-card__caption');
 const createCardButton = document.querySelector('.create-card-button'); // кнопка "создать" карточку с фото
 const elementsTemplate = document.querySelector('#elements__template').content; // получаем контент темплейт
 const element = elementsTemplate.querySelector('.elements__item').cloneNode(true);
-const elementsList = document.querySelector('.elements');
-const formItemLink = document.querySelector('.form__item_type_link');
-const formItemName = document.querySelector('.form__item_type_photo');
-const addCardForm = document.querySelector('.card-form');
+const elementsList = document.querySelector('.elements'); // контейнер для рендера карточек
+const formItemLink = document.querySelector('.form__item_type_link'); // инпут с ссылкой на фото
+const formItemName = document.querySelector('.form__item_type_photo'); // инпут с подписью к фото
+const addCardForm = document.querySelector('.card-form'); // форма создания карточки
+const closePhotoButton = document.querySelector('.photo__close-button'); // кнопка закрытия полноразмерного изображения
 
 
-profileButton.addEventListener('click', () => openPopup(profilePopup));
-profileCloseButton.addEventListener('click', () => closePopup(profilePopup));
-profileSubmitButton.addEventListener('click', submitProfileForm);
-addPhotoButton.addEventListener('click', () => openPopup(addPhotoPopup));
-closePopupButton.addEventListener('click', () => closePopup(addPhotoPopup));
-addCardForm.addEventListener('submit', submitCardForm);
-
+// функции
 function openPopup(popup) { //  открыть модальное окно
   popup.classList.add('popup_opened');
-  formName.value = profileName.textContent;
-  formJob.value = profileCaption.textContent;
 };
 function closePopup(popup) { // закрыть модальное окно
   popup.classList.remove('popup_opened');
-  formItemLink.value = '';
-  formItemName.value = '';
 };
 function submitProfileForm(event) { // отправить данные в профайл
   event.preventDefault();
@@ -71,48 +62,36 @@ function submitProfileForm(event) { // отправить данные в про
   profileCaption.textContent = formJob.value;
   closePopup(profilePopup);
 };
-
-function createCard(data) {
+function createCard(data) { // функция создания карточки
   const element = elementsTemplate.querySelector('.elements__item').cloneNode(true);
   element.querySelector('.elements__caption').textContent = data.name;
   element.querySelector('.elements__img').src = data.link;
   element.querySelector('.elements__img').alt = data.name;
-
-  return element;
-};
-
-function renderCard(card, container) {
-  container.prepend(card);
-  const deleteButton = document.querySelector('.delete-button'); // удаление созданной карточки
+  const deleteButton = element.querySelector('.delete-button'); // удаление созданной карточки
   deleteButton.addEventListener('click', function () {
     const elementItem = deleteButton.closest('.elements__item');
     elementItem.remove();
   });
-  const likeButton = document.querySelector('.like-button');
+  const likeButton = element.querySelector('.like-button');
   likeButton.addEventListener('click', function () {
     likeButton.classList.toggle('like-button_active'); // функция лайка созданной карточки
   });
-
   function openImage(evt) {
     openedImage.src = evt.target.getAttribute('src');
     openedImageCaption.textContent = evt.target.getAttribute('alt');
     imagePopup.classList.add('photo-card_opened');
   };
-
-  const image = document.querySelector('.elements__img');
-  image.addEventListener('click', openImage); // открытие изображения в полном размере
-
-  const closeButton = document.querySelector('.photo__close-button');
-  closeButton.addEventListener('click', function () {
-    imagePopup.classList.remove('photo-card_opened');
-    // работоспособность кнопки закрытия модального окна
-  });
+  const image = element.querySelector('.elements__img');
+  image.addEventListener('click', openImage);
+  return element;
 };
-
+function renderCard(card, container) {
+  container.prepend(card); // отрисовка карточки в контейнере
+};
 function submitCardForm(evt) {
   evt.preventDefault();
   data = {
-    name: document.querySelector('.form__item_type_photo').value,
+    name: document.querySelector('.form__item_type_photo').value, // сабмит формы добавления карточки
     link: document.querySelector('.form__item_type_link').value
   };
   const card = createCard(data);
@@ -121,9 +100,27 @@ function submitCardForm(evt) {
   formItemLink.value = '';
   formItemName.value = '';
 };
-
 initialCards.forEach(function (item) {
   const card = createCard(item);
-  renderCard(card, elementsList);
+  renderCard(card, elementsList);  // отрисовка карточек из массива
 });
 
+
+// слушатели событий
+profileButton.addEventListener('click', (function() {
+  formName.value = profileName.textContent;
+  formJob.value = profileCaption.textContent;
+  openPopup(profilePopup);
+}));
+profileCloseButton.addEventListener('click', () => closePopup(profilePopup));
+profileSubmitButton.addEventListener('click', submitProfileForm);
+addPhotoButton.addEventListener('click', () => openPopup(addPhotoPopup));
+closePopupButton.addEventListener('click', () => {
+  formItemLink.value = '';
+  formItemName.value = '';
+  closePopup(addPhotoPopup)
+});
+addCardForm.addEventListener('submit', submitCardForm);
+closePhotoButton.addEventListener('click', function () {
+  imagePopup.classList.remove('photo-card_opened');
+});
