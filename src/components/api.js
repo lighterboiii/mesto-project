0bda8db3c885810ffcfdff51592f1aa3b387cf33
Vyacheslav Baumtrok <s.baumtrok2009@yandex.ru>
2022-10-17 18:43:03 +0300

    refactor: added api & userInfo classes

--- src/components/api.js
+++ src/components/api.js
@@ -1,5 +1,93 @@
 import { checkResponse } from "../components/utils.js";
 
+export class Api {
+  constructor(options) {
+    this._baseUrl = options.baseUrl;
+    this._headers = options.headers;
+  }
+
+  getInitialCards() {
+    return fetch(`${this._baseUrl}/cards`, {  // рендер карточек с сервера
+      headers: this._headers
+    })
+      .then(checkResponse)
+  };
+
+  getUserInfo() {
+    return fetch(`${this._baseUrl}/users/me`, {  // получение информации пользователя
+      headers: this._headers
+    })
+      .then(checkResponse)
+  };
+
+  setUserInfo(name, caption) {
+    return fetch(`${this._baseUrl}/users/me`, { // отправка информации пользователя на сервер
+      method: 'PATCH',
+      headers: this._headers,
+      body: JSON.stringify({
+        name: name,
+        about: caption
+      })
+    })
+      .then(checkResponse)
+  };
+
+  postCard(data) {
+    return fetch(`${this._baseUrl}/cards`, { // пост карточки через форму
+      method: 'POST',
+      headers: this._headers,
+      body: JSON.stringify({
+        name: data.name,
+        link: data.link
+      })
+    })
+      .then(checkResponse)
+  };
+
+  setLike(data) { // постановка лайка
+    return fetch(`${config.baseUrl}/cards/likes/${data._id}`, {
+      method: 'PUT',
+      headers: config.headers
+    })
+      .then(checkResponse)
+  };
+
+  deleteLike(data) {  // снятие лайка
+    return fetch(`${this._baseUrl}/cards/likes/${data._id}`, {
+      method: 'DELETE',
+      headers: this._headers
+    })
+      .then(checkResponse)
+  };
+
+  setAvatar (avatar) {
+    return fetch(`${config.baseUrl}/users/me/avatar`, { // отправка аватара на сервер (в классе)
+      method: 'PATCH',
+      headers: config.headers,
+      body: JSON.stringify({
+        avatar: avatar
+      })
+    })
+      .then(checkResponse)
+  };
+
+  deleteCard(data) {
+    return fetch(`${this._baseUrl}/cards/${data}`, { // удаление карточки
+      method: 'DELETE',
+      headers: this._headers
+    })
+      .then(checkResponse)
+  };
+}
+// const api = new Api({
+//   baseUrl: 'https://nomoreparties.co/v1/cohort-42',
+//   headers: {
+//     authorization: 'c56e30dc-2883-4270-a59e-b2f7bae969c6',
+//     'Content-Type': 'application/json'
+//   }
+// });
+
+// отсюда удалить после рефактора
 const config = {
   baseUrl: 'https://nomoreparties.co/v1/plus-cohort-15',
   headers: {
@@ -9,14 +97,14 @@ const config = {
 };
 
 const getUserInfo = () => {
-  return fetch(`${config.baseUrl}/users/me`, {  // получение информации пользователя
+  return fetch(`${config.baseUrl}/users/me`, {  // получение информации пользователя (в классе)
     headers: config.headers
   })
     .then(checkResponse)
 };
 
 const setUserInfo = (name, caption) => {
-  return fetch(`${config.baseUrl}/users/me`, { // отправка информации пользователя на сервер
+  return fetch(`${config.baseUrl}/users/me`, { // отправка информации пользователя на сервер (в классе)
     method: 'PATCH',
     headers: config.headers,
     body: JSON.stringify({
@@ -28,14 +116,14 @@ const setUserInfo = (name, caption) => {
 };
 
 const getInitialCards = () => {
-  return fetch(`${config.baseUrl}/cards`, {  // рендер карточек с сервера
+  return fetch(`${config.baseUrl}/cards`, {  // рендер карточек с сервера (в классе)
     headers: config.headers
   })
     .then(checkResponse)
 };
 
 const postCard = (name, link) => {
-  return fetch(`${config.baseUrl}/cards`, { // пост карточки через форму
+  return fetch(`${config.baseUrl}/cards`, { // пост карточки через форму (в классе)
     method: 'POST',
     headers: config.headers,
     body: JSON.stringify({
@@ -47,7 +135,7 @@ const postCard = (name, link) => {
 };
 
 const setAvatar = (avatar) => {
-  return fetch(`${config.baseUrl}/users/me/avatar`, { // отправка аватара на сервер
+  return fetch(`${config.baseUrl}/users/me/avatar`, { // отправка аватара на сервер (в классе)
     method: 'PATCH',
     headers: config.headers,
     body: JSON.stringify({
@@ -57,7 +145,7 @@ const setAvatar = (avatar) => {
     .then(checkResponse)
 };
 
-const setLike = (data) => { // постановка лайка
+const setLike = (data) => { // постановка лайка (в классе)
   return fetch(`${config.baseUrl}/cards/likes/${data._id}`, {
     method: 'PUT',
     headers: config.headers
@@ -65,7 +153,7 @@ const setLike = (data) => { // постановка лайка
     .then(checkResponse)
 };
 
-const deleteLike = (data) => {  // снятие лайка
+const deleteLike = (data) => {  // снятие лайка (в классе)
   return fetch(`${config.baseUrl}/cards/likes/${data._id}`, {
     method: 'DELETE',
     headers: config.headers
@@ -75,7 +163,7 @@ const deleteLike = (data) => {  // снятие лайка
 
 
 const deleteCard = (data) => {
-  return fetch(`${config.baseUrl}/cards/${data}`, { // удаление карточки
+  return fetch(`${config.baseUrl}/cards/${data}`, { // удаление карточки (в классе)
     method: 'DELETE',
     headers: config.headers
   })
