8bc1a425cc244a2aa5029b7522101c7408e2d166
Kirill Semenov <semenovkirill1244@gmail.com>
2022-10-28 09:59:12 +0300

    fix/card

--- src/components/card.js
+++ src/components/card.js
@@ -1,5 +1,10 @@
 export default class Card {
-  constructor(data, userID, { handleClickImage, handleDeleteClick, handleLikeClick }, cardTemplate) {
+  constructor(
+    data,
+    userID,
+    { handleClickImage, handleDeleteClick, handleLikeClick },
+    cardTemplate
+  ) {
     this._name = data.name;
     this._link = data.link;
     this._likesCount = data.likes.length;
@@ -43,14 +48,11 @@ export default class Card {
   haveLikeOwner() {
     return this._likes.some((like) => {
       return like._id === this._userID;
-    })
+    });
   }
 
   setStateLike() {
-    this._card
-        .querySelector(".like-button")
-        .classList
-        .toggle("like-button_active");
+    this._like.classList.toggle("like-button_active");
   }
 
   setCountLikeToPage(likes) {
@@ -59,39 +61,36 @@ export default class Card {
   }
 
   setEventListeners() {
-    const like = this._card.querySelector(".like-button");
-    const image = this._card.querySelector(".elements__img");
-
-    like.addEventListener("click", () => {
-      this._addHandlerLike(like);
+    this._like.addEventListener("click", () => {
+      this._addHandlerLike();
     });
     this._btnRemove.addEventListener("click", () => {
       this._addHandlerBtnRemove();
     });
-    image.addEventListener("click", () => {
+    this._image.addEventListener("click", () => {
       this._handleClickImage();
     });
   }
 
-
   createCard() {
     this._getTemplate();
 
-    const elementImage = this._card.querySelector(".elements__img");
-    const elementTitle = this._card.querySelector(".elements__caption");
-    const elementLikes = this._card.querySelector(".like-count");
+    this._like = this._card.querySelector(".like-button");
+    this._image = this._card.querySelector(".elements__img");
+    this._elementTitle = this._card.querySelector(".elements__caption");
+    this._elementLikes = this._card.querySelector(".like-count");
     this._btnRemove = this._card.querySelector(".delete-button");
 
-    elementImage.src = this._link;
-    elementImage.alt = this._name;
-    elementTitle.textContent = this._name;
-    elementLikes.textContent = this._likesCount;
+    this._image.src = this._link;
+    this._image.alt = this._name;
+    this._elementTitle.textContent = this._name;
+    this._elementLikes.textContent = this._likesCount;
 
     if (this._ownerId === this._userID) {
-      this._btnRemove.style.visibility = 'visible';
+      this._btnRemove.style.visibility = "visible";
     }
 
-    if (this.haveLikeOwner() ) {
+    if (this.haveLikeOwner()) {
       this.setStateLike();
     }
 
@@ -100,4 +99,3 @@ export default class Card {
     return this._card;
   }
 }
-
